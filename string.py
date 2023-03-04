78dddcf149f6b520d6297dedd3d8e994d3e01bdd
aljaneayoub <ayoub.aljane@tek-up.de>
2023-03-04 22:14:48 +0000

    this commit just for push uing the token

---
+++ string.py
@@ -0,0 +1,12 @@
+import sys
+import time
+letter="AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopmlkjhgfdqswxcvbn"
+number="0123456789"
+if __name__ == "__main__":
+    print("you can\'t execute this file !")
+    time.sleep(1)
+    print("execute password_gen.py")
+    time.sleep(1)
+    print("bye")
+    sys.exit()
+
